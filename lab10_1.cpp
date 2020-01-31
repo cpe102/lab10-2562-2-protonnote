22eaf158266338a1629ae035786a044ac5d64d6d
lab314-7@labeng.local <lab314-7@labeng.local>
2020-01-31 15:34:56 +0700

    send

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,21 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+
+int main (){
+    int i=1;
+    string y;
+    string a[]={"A","B+","C+","C","D+","D","F","W"};
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    srand (time(0));
+    int b = rand()%8;
+    cout << "You will get "<< a[b] << " in this 261102.";
+    return 0;
+}
